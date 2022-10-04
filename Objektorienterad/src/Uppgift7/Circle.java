2fcfd3bf8d6bc122eb6a23698cd60d98cbe139fa
knottem <59744487+knottem@users.noreply.github.com>
2022-10-04 17:01:34 +0200

    lekte lite med inlämningsuppgiften

---
+++ Objektorienterad/src/Uppgift7/Circle.java
@@ -0,0 +1,16 @@
+package Uppgift7;
+
+public class Circle extends Figure{
+
+    int radius;
+
+    public Circle(int startx, int starty, int radius) {
+        super(new Point(startx, starty));
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ Objektorienterad/src/Uppgift9/RomanNumeral.java
@@ -0,0 +1,29 @@
+package Uppgift9;
+
+public enum RomanNumeral {
+    I("one", 1),
+    V("five", 5),
+    X("ten", 10),
+    L("fifty", 50),
+    C("a hundred", 100),
+    D("fivehundred", 500),
+    M("a thousand", 1000)
+    ;
+
+    private final String numeral;
+    private final int number;
+
+    RomanNumeral(String numeral, int number) {
+        this.numeral = numeral;
+        this.number = number;
+    }
+
+    public String getNumeral() {
+          return numeral;
+      }
+
+      public int getNumber() {
+          return number;
+      }
+
+}
