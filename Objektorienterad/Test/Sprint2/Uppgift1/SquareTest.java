f6363bc6f3d5f242eb4763e53cd7f0e3bd5d8455
knottem <59744487+knottem@users.noreply.github.com>
2022-10-07 14:15:18 +0200

    testa olika assert

--- Objektorienterad/Test/Sprint2/Uppgift1/SquareTest.java
+++ Objektorienterad/Test/Sprint2/Uppgift1/SquareTest.java
@@ -6,18 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SquareTest{
 
-    int width = 3;
-    int length = 3;
 
+    Square s1 = new Square(3,3);
+
+    @Test
+    void Test(){
+        assertEquals(3, s1.getLength());
+        assertNotEquals(4, s1.getLength());
+        assertTrue(s1.getWidth() == 3);
+        assertFalse(s1.getWidth() == 4);
+
+    }
 
     @Test
     void getAreaTest() {
-        assert(width*length == 9);
+        assert(s1.getArea() == 9);
 
     }
 
     @Test
     void getCircumferenceTest() {
-        assert(width*2 + length*2 == 12);
+        assert(s1.getCircumference() == 12);
     }
 }
--- Objektorienterad/src/Sprint2/Uppgift1/Square.java
+++ Objektorienterad/src/Sprint2/Uppgift1/Square.java
@@ -2,8 +2,29 @@ package Sprint2.Uppgift1;
 
 public class Square implements Figure{
 
-    int length;
-    int width;
+    protected int length;
+    protected int width;
+
+    public Square(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
 
     @Override
     public double getArea() {
