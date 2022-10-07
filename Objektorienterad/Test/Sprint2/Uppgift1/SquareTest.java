a4704c2459e5d18705a0ee175b9bea35885f998f
knottem <59744487+knottem@users.noreply.github.com>
2022-10-07 14:18:47 +0200

    testa olika assert

--- Objektorienterad/Test/Sprint2/Uppgift1/SquareTest.java
+++ Objektorienterad/Test/Sprint2/Uppgift1/SquareTest.java
@@ -10,22 +10,22 @@ class SquareTest{
     Square s1 = new Square(3,3);
 
     @Test
-    void Test(){
+    void testGet(){
         assertEquals(3, s1.getLength());
         assertNotEquals(4, s1.getLength());
         assertEquals(3, s1.getWidth());
         assertNotEquals(4, s1.getWidth());
-
     }
 
     @Test
     void getAreaTest() {
-        assert(s1.getArea() == 9);
+        assertEquals(9,s1.getArea());
 
     }
 
     @Test
     void getCircumferenceTest() {
-        assert(s1.getCircumference() == 12);
+        assertEquals(12,s1.getCircumference());
+        assertNotEquals(13,s1.getCircumference()); //felaktig testar
     }
 }
