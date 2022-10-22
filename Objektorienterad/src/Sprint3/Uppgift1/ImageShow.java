86d5c7703e8d7aacd6ac2e977c5319274e7e4561
knottem <59744487+knottem@users.noreply.github.com>
2022-10-23 00:56:07 +0200

    uppgift 1 sprint 3

--- Objektorienterad/src/Sprint3/Uppgift1/ImageShow.java
+++ Objektorienterad/src/Sprint3/Uppgift1/ImageShow.java
@@ -1,12 +1,9 @@
 package Sprint3.Uppgift1;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.util.ArrayList;
 import javax.swing.*;
 
@@ -47,14 +44,12 @@ public class ImageShow extends JFrame implements ActionListener {
     void listFiles(Path path) throws IOException {
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
             for (Path entry : stream) {
-                if (Files.isDirectory(entry)) {
-                    listFiles(entry);
-                }
                 files.add(entry.toString());
                 counter++;
             }
         }
     }
+
 
     public static void main(String[] args) throws IOException {
         ImageShow imageShow = new ImageShow();
