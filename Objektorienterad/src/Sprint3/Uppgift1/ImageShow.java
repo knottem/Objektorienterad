7ad65eb2fb8a16f6486cb7dcfea907eb760e4d26
knottem <59744487+knottem@users.noreply.github.com>
2022-10-23 00:02:15 +0200

    uppgift 1 spring 3

---
+++ Objektorienterad/src/Sprint3/Uppgift1/ImageShow.java
@@ -0,0 +1,63 @@
+package Sprint3.Uppgift1;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class ImageShow extends JFrame implements ActionListener {
+
+    JButton changeImageButton;
+    JLabel imageViewer;
+    JPanel panel;
+    final ArrayList<String> files = new ArrayList<>();
+    int imageIndex = 0;
+    int counter = 0;
+
+    public void imageShow() throws IOException {
+
+        listFiles(Path.of("Objektorienterad/src/Sprint3/Uppgift1/pics"));
+
+        changeImageButton = new JButton("Change picture");
+        changeImageButton.addActionListener(this);
+
+        imageViewer = new JLabel(new ImageIcon(new ImageIcon(files.get(imageIndex)).getImage().getScaledInstance(1024,768, Image.SCALE_DEFAULT)));
+
+        panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.setBackground(Color.BLACK);
+        panel.add(imageViewer);
+        panel.add(changeImageButton);
+        this.add(panel);
+
+        setSize(1024,900);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    public void actionPerformed(ActionEvent e) {
+        imageIndex = (imageIndex + 1) % counter;
+        imageViewer.setIcon(new ImageIcon(new ImageIcon(files.get(imageIndex)).getImage().getScaledInstance(1024,768, Image.SCALE_DEFAULT)));
+    }
+
+    void listFiles(Path path) throws IOException {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    listFiles(entry);
+                }
+                files.add(entry.toString());
+                counter++;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ImageShow imageShow = new ImageShow();
+        imageShow.imageShow();
+    }
+}
