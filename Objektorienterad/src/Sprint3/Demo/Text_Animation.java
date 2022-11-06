47dc3e92033b36639e3cf8726f6fa4bee094b2a0
knottem <59744487+knottem@users.noreply.github.com>
2022-11-06 16:16:37 +0100

    började lite på sprint 4

---
+++ Objektorienterad/src/Sprint3/Demo/Text_Animation.java
@@ -0,0 +1,90 @@
+package Sprint3.Demo;
+
+import java.awt.Color;
+import java.awt.Font;
+import static java.awt.Font.BOLD;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Text_Animation extends JPanel{
+
+    int x,x2,x3;
+    int bannerSize = 20;
+    int y = bannerSize;
+    int y2 = bannerSize;
+    int y3 = 0;
+    static int tileSize = 100;
+
+    public static int totalSize = tileSize*4;
+
+     public void paint(Graphics gp) {
+
+        super.paint(gp);
+        Graphics2D g2d= (Graphics2D) gp;
+        g2d.setColor(Color.red);
+        g2d.setFont(new Font("BOLD", BOLD, 35));
+
+        String stringA = "Testing Testing";
+        int length = g2d.getFontMetrics().stringWidth(stringA);
+        int height = g2d.getFontMetrics().getHeight();
+        g2d.drawString(stringA, x, y);
+
+
+        try {
+            if(x == x2){
+                x+=2;
+                x2+=2;
+                if(x2 + length >= totalSize){
+                    x3 = totalSize - length;
+                    x2 = totalSize + 5000;
+                }
+            }
+            if(y == y2){
+                y+=2;
+                y2+=2;
+                if(y2 + height >= totalSize){
+                    y3 = totalSize - height;
+                    y = totalSize - height;
+                    y2 = totalSize + 5000;
+                }
+            }
+            if(x == x3) {
+                x-=2;
+                x3-=2;
+                if(x == 0){
+                    x2 = 0;
+                }
+            }
+            if(y == y3){
+                y-=2;
+                y3-=2;
+                if(y <= bannerSize){
+                    y2 = bannerSize;
+                    y = bannerSize;
+                }
+            }
+            Thread.sleep(5);
+
+            repaint();
+
+        } catch (InterruptedException ex) {
+            JOptionPane.showMessageDialog(this, ex);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        JFrame jf = new JFrame();
+        jf.setSize(totalSize, totalSize);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setResizable(false);
+        jf.add(new Text_Animation());
+        jf.setLocationRelativeTo(null);
+        jf.setVisible(true);
+    }
+
+}
--- Objektorienterad/src/Sprint4/Uppgift1/Server.java
+++ Objektorienterad/src/Sprint4/Uppgift1/Server.java
@@ -1,6 +1,4 @@
 package Sprint4.Uppgift1;
-
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
