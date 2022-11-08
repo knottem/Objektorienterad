15c50a1673fff28d4eb464cbde44495c0a27842b
knottem <walle.o.w@gmail.com>
2022-11-08 13:53:20 +0100

    uppgift 14

--- Objektorienterad/src/Sprint4/Uppgift4/Sensor.java
+++ Objektorienterad/src/Sprint4/Uppgift4/Sensor.java
@@ -1,36 +1,70 @@
 package Sprint4.Uppgift4;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.util.Scanner;
+import java.net.*;
 
 public class Sensor {
 
-    private void send() throws IOException {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Vilken stad?");
-        String city = scan.nextLine();
-        System.out.println("Vilken tempereratur?");
-        String temp = scan.nextLine();
+    JFrame frame = new JFrame("Sensor Program;");
+    JPanel topPanel = new JPanel();
+    JPanel bottomPanel = new JPanel();
+    JLabel cityLabel = new JLabel("Stad:");
+    JLabel tempLabel = new JLabel("Temperatur:");
+    JTextField cityText = new JTextField();
+    JTextField tempText = new JTextField();
+    JButton button = new JButton("Skicka");
 
-        DatagramSocket datagramSocket = new DatagramSocket();
+    private void send() {
 
-        while(city != null || temp != null){
-            String message = city + temp;
-            byte[] data = message.getBytes();
-            DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName(adress), 45678);
-            datagramSocket.send(packet);
-            city = null;
-            temp = null;
-        }
+            button.addActionListener(e -> {
+                String city = cityText.getText();
+                String temp = tempText.getText();
+                try (DatagramSocket datagramSocket = new DatagramSocket()) {
+                    String message = city + " " + temp;
+                    byte[] data = message.getBytes();
+                    DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName("localhost"), 23456);
+                    try {
+                        datagramSocket.send(packet);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                } catch (SocketException | UnknownHostException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
     }
+
+
+
+    public void setUpWindow(){
+
+        topPanel.add(cityLabel);
+        topPanel.add(cityText);
+        topPanel.setLayout(new GridLayout(1,1));
+
+        bottomPanel.add(tempLabel);
+        bottomPanel.add(tempText);
+        bottomPanel.setLayout(new GridLayout(1,1));
+
+        frame.setLayout(new GridLayout(3,1));
+        frame.add(topPanel, 0);
+        frame.add(bottomPanel,1);
+        frame.add(button,2);
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+        frame.pack();
+    }
+
 
 
     public static void main(String[] args) throws IOException {
         Sensor sensors = new Sensor();
+        sensors.setUpWindow();
         sensors.send();
     }
 }
--- Objektorienterad/src/Sprint4/Uppgift4/WeatherServer.java
+++ Objektorienterad/src/Sprint4/Uppgift4/WeatherServer.java
@@ -3,18 +3,38 @@ package Sprint4.Uppgift4;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.ArrayList;
 
 public class WeatherServer {
 
+    static ArrayList<String> city = new ArrayList<>();
+    static ArrayList<String> temp = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
-        int minPort = 45678;
-        DatagramSocket socket = new DatagramSocket(minPort);
-        byte[] data = new byte[256];
-        while(true){
-            DatagramPacket packet = new DatagramPacket(data, data.length);
-            socket.receive(packet);
-            String message = new String(packet.getData(), 0, packet.getLength());
-            System.out.println(message);
+        try (DatagramSocket socket = new DatagramSocket(23456)) {
+            byte[] data = new byte[256];
+            while (true) {
+
+                DatagramPacket packet = new DatagramPacket(data, data.length);
+                socket.receive(packet);
+                String message = new String(packet.getData(), 0, packet.getLength());
+                String[] messageSplit = message.split(" ", 2);
+
+                if(messageSplit.length == 2) {
+                    city.add(messageSplit[0]);
+                    temp.add(messageSplit[1]);
+                    System.out.println("Temperatur för staden " + messageSplit[0] + " : " + messageSplit[1]);
+                }
+                else{
+                    System.out.println(message);
+                }
+
+                if (city.size() == 3) {
+                    for (int i = 0; i < city.size(); i++) {
+                        System.out.println(city.get(i) + " " + temp.get(i) + " Grader");
+                    }
+                }
             }
         }
     }
+}
