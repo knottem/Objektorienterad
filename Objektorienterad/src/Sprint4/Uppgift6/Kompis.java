112448de907b0bcb6b509fd3b702ecf73c68178e
knottem <59744487+knottem@users.noreply.github.com>
2022-11-09 16:21:34 +0100

    start uppgift 6

--- Objektorienterad/src/Sprint4/Uppgift6/Kompis.java
+++ Objektorienterad/src/Sprint4/Uppgift6/Kompis.java
@@ -3,8 +3,8 @@ package Sprint4.Uppgift6;
 public class Kompis {
 
     private final String name, email;
-    private int phoneNumber;
-    private long dateOfBirth;
+    private final int phoneNumber;
+    private final long dateOfBirth;
 
     public Kompis(String name, String email, int phoneNumber, long dateOfBirth) {
         this.name = name;
@@ -14,26 +14,6 @@ public class Kompis {
     }
     public String getName() {
         return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public long getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(long dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
     }
 
     @Override
